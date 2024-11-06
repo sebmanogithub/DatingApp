b5b4e70f2315eed0bf38cff02d56b39ee526a428
sebmanogithub <sebastiengithub@gmail.com>
2024-11-06 11:18:34 +0100

    Ajout du client Angular

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using API.Data;
 using API.Entities;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
@@ -17,16 +18,17 @@ namespace API.Controllers
         {
             _context = context;
         }
+
         [HttpGet]
-        public ActionResult<IEnumerable<AppUser>> GetUsers()
+        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
         {
-            return _context.Users.ToList();
+            return await _context.Users.ToListAsync();
         }
 
         [HttpGet("{id}")]
-        public ActionResult<AppUser> GetUser(int id)
+        public async Task<ActionResult<AppUser>> GetUser(int id)
         {
-            return _context.Users.Find(id);
+            return await _context.Users.FindAsync(id);
         }
     }
 }
