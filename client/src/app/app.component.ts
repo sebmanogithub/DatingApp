32fec55bacdedae286dc2534885088f74231804d
sebmanogithub <sebastiengithub@gmail.com>
2024-11-06 15:37:56 +0100

    Affichage de la première API sur l'interface client

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 // importe le décorateur Component d'angular nécessaire pour créer un composant Angular.
 
 @Component({
@@ -8,6 +9,23 @@ import { Component } from '@angular/core';
 })
 //Ce composant AppComponent est le composant principal d'une application 
 // Angular (selector: 'app-root') et sert de point d'entrée.
-export class AppComponent {
+export class AppComponent implements OnInit
+{
   title = 'The Dating App';
+  users: any; // No type safety
+
+  constructor(private http: HttpClient){}
+
+  ngOnInit() {
+    this.getUsers();
+  };
+
+  getUsers() {
+    // return an observable
+    this.http.get('https://localhost:5001/api/users').subscribe(response => {
+      this.users = response;
+    }, error => {
+      console.log(error);
+    });
+  }
 }
