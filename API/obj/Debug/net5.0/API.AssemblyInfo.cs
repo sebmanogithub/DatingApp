1cbb58d01756b44ffb60e86e7634b6ce836b6e2d
sebmanogithub <sebastiengithub@gmail.com>
2024-12-07 09:16:57 +0100

    Ajout de la table Photo et de la relation avec AppUser

--- API/obj/Debug/net5.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net5.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2d4325c2a175a47d72dcffb9aacc72c27c5a0cea")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b7c71d420b532caae63d4c04bac213cd3e7f00c2")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
